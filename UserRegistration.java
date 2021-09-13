aa4701882e9024ac7f2a0653fdf25f404a9fee7f
NayanPunyarthi <punyarthinayan16@gmail.com>
2021-09-13 22:09:23 +0530

    Email

--- UserRegistration.java
+++ UserRegistration.java
@@ -1,33 +1,47 @@
 
-//Last Name
+/Valid Email ID
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+//This is a User Resgistration Problem where the code needs to ensure that all the validations are in place during user entry
 
 //Created a class for user inputs and validation
-public class UserRegestration {
-    void regestration(){
+public class UserRegistration {
+    void registration() {
         Scanner scan = new Scanner(System.in);
         System.out.println("Enter Your Name");
         String name = scan.next();
         System.out.println("Enter Your Last Name");
         String lastName = scan.next();
+        System.out.println("Enter Your email");
+        String email = scan.next();
 
         String nameRegex = "^[A-Z]{1}[a-z]{2,15}$";
+        String emailRegex = "^[a-z]{3}[a-zA-Z0-9+_.-]+@[a-z]{2}[.]{1}[a-z]{2}[.][a-z]+$";
+
         Pattern pattern = Pattern.compile(nameRegex);
 
         Matcher matcher = pattern.matcher(name);
         Matcher matcher1 = pattern.matcher(lastName);
-        System.out.println("valid first Name: "+matcher.matches());
-        System.out.println("valid last Name: "+matcher1.matches());
+
+        System.out.println("valid first Name: " + matcher.matches());
+        System.out.println("valid last Name: " + matcher1.matches());
+
+        boolean result = email.matches(emailRegex);
+        if (result) {
+            System.out.println("Given email-id is valid");
+        } else {
+            System.out.println("Given email-id is not valid");
+        }
     }
 }
+
 
 //Main class to call out functions
 class Main{
     public static void main(String[] args) {
-        UserRegestration user = new UserRegestration();
-        user.regestration();
+        UserRegistration user = new UserRegistration();
+        user.registration();
     }
 }
