dbe1af1bc41776f8e4d542c341ab16bdb5e8e98b
NayanPunyarthi <punyarthinayan16@gmail.com>
2021-09-13 22:03:24 +0530

    Last Name

--- UserRegistration.java
+++ UserRegistration.java
@@ -1,32 +1,33 @@
 
-//First Name
+//Last Name
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-//This is a User Resgistration Problem where the code needs to ensure that all the validations are in place during user entry
 
 //Created a class for user inputs and validation
-public class UserRegistration {
-    void registration(){
+public class UserRegestration {
+    void regestration(){
         Scanner scan = new Scanner(System.in);
         System.out.println("Enter Your Name");
         String name = scan.next();
+        System.out.println("Enter Your Last Name");
+        String lastName = scan.next();
 
         String nameRegex = "^[A-Z]{1}[a-z]{2,15}$";
         Pattern pattern = Pattern.compile(nameRegex);
 
         Matcher matcher = pattern.matcher(name);
-        System.out.println("valid input: "+matcher.matches());
+        Matcher matcher1 = pattern.matcher(lastName);
+        System.out.println("valid first Name: "+matcher.matches());
+        System.out.println("valid last Name: "+matcher1.matches());
     }
 }
 
 //Main class to call out functions
 class Main{
     public static void main(String[] args) {
-        UserRegistration user = new UserRegistration();
-        user.registration();
+        UserRegestration user = new UserRegestration();
+        user.regestration();
     }
 }
-
-
